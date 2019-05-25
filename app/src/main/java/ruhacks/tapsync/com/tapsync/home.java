0f7386843f8f64ded1a37f919433d98f18484c2c
Hrithik Shah <hrithikchamp@gmail.com>
2019-05-25 10:02:06 -0400

    played around with nfc

--- app/src/main/java/ruhacks/tapsync/com/tapsync/home.java
+++ app/src/main/java/ruhacks/tapsync/com/tapsync/home.java
@@ -1,17 +1,19 @@
 package ruhacks.tapsync.com.tapsync;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -20,6 +22,7 @@ public class home extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.d("MainActivity", "Hello World");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -41,8 +44,6 @@ public class home extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
-
-    break;
 
     @Override
     public void onBackPressed() {
@@ -99,5 +100,11 @@ public class home extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void launchNFC(View view) {
+        Log.d("FloatActionButton", "Launching NFC!");
+        Intent intent = new Intent(this, nfcPage.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/ruhacks/tapsync/com/tapsync/nfcPage.java
@@ -0,0 +1,13 @@
+package ruhacks.tapsync.com.tapsync;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nfcPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc_page);
+    }
+}
